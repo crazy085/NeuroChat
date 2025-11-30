f6970f0ffd8a102fa9c215e924957dbc72e340a6
crazy085 <crazythoughts085@gmail.com>
2025-11-30 20:51:56 +0530

    Update darkmode.js

--- frontend/js/darkmode.js
+++ frontend/js/darkmode.js
@@ -2,3 +2,5 @@ const toggleBtn = document.getElementById('toggle-dark');
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+if (Notification.permission !== 'granted') Notification.requestPermission();
