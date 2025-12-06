3735da324e0ece602a86f991316af094226c65b8
crazy085 <crazythoughts085@gmail.com>
2025-12-06 21:12:54 +0530

    Initialize Express server with CORS and static files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// For any request that doesn't match a static file, serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`NeuroChat server running on port ${PORT}`);
+});
